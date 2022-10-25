6d70481a8f27296d89733d251f453cc2e88efade
Braian Jassan <braianjassan@gmail.com>
2022-10-25 14:56:26 -0300

    apply navbar in main layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  resources :brands
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
 
   resources :products
+  resources :brands
+  
 end
